3bc5d96e3635e0dfb9389ed6746c48cd5b59929d
Maël Nison <nison.mael@gmail.com>
2025-10-18 00:49:01 +0200

    Avoids relayout flash

--- documentation/src/components/HeroVersions.tsx
+++ documentation/src/components/HeroVersions.tsx
@@ -7,8 +7,14 @@ const queryClient = new QueryClient();
 type BadgeProps = {
   labelClass: string;
   label: string;
-  value: string;
+  value: any;
 };
+
+function Version({name}: {name: string}) {
+  const versions = useYarnReleaseVersions();
+
+  return <>{versions[name]}</>;
+}
 
 function Badge({labelClass, label, value}: BadgeProps) {
   return (
@@ -16,20 +22,20 @@ function Badge({labelClass, label, value}: BadgeProps) {
       <div class={`rounded-l-lg px-2 ${labelClass}`}>
         {label}
       </div>
-      <div class={`rounded-r-lg px-2 bg-linear-to-t from-gray-950 to-gray-800 `}>
-        {value}
+      <div class={`rounded-r-lg w-16 px-2 bg-linear-to-t from-gray-950 to-gray-800 text-center`}>
+        <Suspense fallback={<></>}>
+          {value}
+        </Suspense>
       </div>
     </div>
   );
 }
 
 function Versions() {
-  const {stable, canary} = useYarnReleaseVersions();
-
   return (
     <div class={`flex items-center gap-x-2`}>
-      <Badge labelClass="bg-linear-to-t from-green-800 to-green-600 text-white" label="Stable" value={stable} />
-      <Badge labelClass="bg-linear-to-t from-orange-900 to-orange-800 text-white" label="Canary" value={canary} />
+      <Badge labelClass="bg-linear-to-t from-green-800 to-green-600 text-white" label="Stable" value={<Version name="stable" />} />
+      <Badge labelClass="bg-linear-to-t from-orange-900 to-orange-800 text-white" label="Canary" value={<Version name="canary" />} />
     </div>
   );
 }
@@ -37,16 +43,7 @@ function Versions() {
 export default function HeroVersions() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense
-        fallback={
-          <div class={`flex items-center gap-x-2`}>
-            <div class={`h-4 w-20 bg-white/20 rounded animate-pulse`} /> |
-            <div class={`h-4 w-20 bg-white/20 rounded animate-pulse`} />
-          </div>
-        }
-      >
-        <Versions />
-      </Suspense>
+      <Versions />
     </QueryClientProvider>
   );
 }
